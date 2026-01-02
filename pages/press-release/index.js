ac7f2898d0eb7d7967b89b1bf14984044745acfa
Neha Giri <neha.giri.inchbrickrealty@gmail.com>
2026-01-02 19:11:13 +0530

    update all

--- pages/press-release/index.js
+++ pages/press-release/index.js
@@ -5,16 +5,16 @@ import PrCard from '@/components/website/press-release/PrCard';
 import { fetchData, fetchPress } from '@/config/fetchApi';
 import API_URLS from '@/config/apiconfig';
 
-const Index = ({press,meta}) => {
+const Index = ({ press, meta }) => {
   return (
     <>
-      <CommonBanner title="Press Release" meta={meta}/>
+      <CommonBanner title="Press Release" meta={meta} />
       <div className='container my-5'>
         <div className='row'>
           {/* Use pressData here */}
           {press && press.map((data, index) => (
             <div className='col-md-3 mb-3' key={index}>
-                <PrCard data={data}/>
+              <PrCard data={data} />
             </div>
           ))}
         </div>
@@ -26,13 +26,13 @@ const Index = ({press,meta}) => {
 export default Index;
 
 Index.getLayout = function getLayout(page) {
-  const  {props} = page;
+  const { props } = page;
   return <Website meta={props.meta}>{page}</Website>;
 };
-export const getServerSideProps = async()=> {
-  const press = await fetchPress({status:1});
-  const meta = await fetchData(API_URLS.META,{slug:'press-release',columns:'title,description,thumbnail,slug'});
+export const getServerSideProps = async () => {
+  const press = await fetchPress({ status: 1 });
+  const meta = await fetchData(API_URLS.META, { slug: 'press-release', columns: 'title,description,thumbnail,slug' });
   return {
-    props:{press,meta:meta.data[0] || null}
+    props: { press, meta: meta.data[0] || null }
   }
 }
